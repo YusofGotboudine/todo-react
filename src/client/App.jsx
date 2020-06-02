53137501104c8a6238baecc605eb4890f8dcbd3c
YusofGotboudine <yusof95@hotmail.com>
2020-06-02 10:15:48 +0800

    Yusof's To Do List

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,14 +1,63 @@
 import React from 'react';
+//import List from './List';
 import { hot } from 'react-hot-loader';
 
-class App extends React.Component {
+var items = {};
+class ListItem extends React.Component {
+
   render() {
     return (
+      <p>{this.props.item}</p>
+    );
+  }
+}
+
+class List extends React.Component {
+
+  render() {
+    let itemsElements = this.props.items.map((item, index) => {
+      return <ListItem item={item}></ListItem>;
+    });
+    return (
+      <ul>
+        {itemsElements}
+      </ul>
+    );
+  }
+}
+class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      clicked: false,
+      word: " "
+    };
+  }
+
+  changeHandler(event) {
+    this.setState({ word: event.target.value });
+    console.log("change", event.target.value);
+  }
+
+  clickHandler() {
+    console.log("Clicked!");
+    items.push(event.target.value);
+    this.setState({ word: ' ' });
+  }
+
+  render() {
+    console.log("Rendering App")
+    return (
       <div>
-        Welcome.
+        <input onChange={(event) => { this.changeHandler(event); }} value={this.state.word}></input>
+        <button onClick={() => { this.clickHandler() }}>Add To List</button>
+        <List item={items} />
       </div>
     );
   }
 }
 
 export default hot(module)(App);
+
---
+++ src/client/List.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class List extends React.Component {
+
+
+
+    render() {
+
+    }
+
+}
+
+export default List;
